ae155e4e8d01c86c9080679a6741c21694b6f90f
romain cadarsi <romain.cadarsi@codein.fr>
2021-01-21 10:28:35 +0100

    bugfix

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -52,7 +52,7 @@ class Reservation {
     setPrix(distance,date) {
             $('#prix').html('...')
             $.ajax({
-                url : 'index.php/xhr/getPrix?infos='+ JSON.stringify({
+                url : '/index.php/xhr/getPrix?infos='+ JSON.stringify({
                     'from' :    this.getAddresseDepart(),
                     'to' : this.getAdresseArrivee(),
                     'h' :  date._d.getHours()
